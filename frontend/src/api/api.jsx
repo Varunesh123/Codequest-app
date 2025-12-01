db516d6929481f29acc7150cf08ed19e593ebd66
Varunesh Pathak <varuneshpathak21@gmail.com>
2025-12-01 21:30:49 +0530

    abc

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://codequest-app-mhzz.onrender.com",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://codequest-app-dhy3.vercel.app/",
 });
 
 export const getContests = () => API.get("/contests");
